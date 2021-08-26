c37088b0d9d642916d04d46abb3462c01d6b5162
ricardodepaula <r.cap@hotmail.com>
2021-08-26 12:53:21 -0300

    initial setup

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import { H1, Wrapper } from '../styles/styles'
+const Index = () => {
+  return (
+    <Wrapper>
+      <H1>hello!!!!</H1>
+    </Wrapper>
+  )
+}
+
+export default Index
---
+++ src/styles/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: var(--max-width);
+`
+
+export const H1 = styled.h1`
+  color: red;
+`
